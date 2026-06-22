f426b7c818d5bef0060b5e2dc0ca885fb0d03c47
Anna Sankova <anya.sankova.05@gmail.com>
2026-06-22 15:42:43 +0600

    Layout Files

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Contact</h1>
-    <p> <b>Author:</b> Icellet</p>
-    <p> <b>Email:</b> test.email@gmail.com</p>
+<x-layout title="Contact Us">
+    <h1>Contact Us</h1>
 
-    <div>
-        <a href="/">Return Home</a>
-    </div>
-</body>
-</html>
+    <x-card class="max-w-400">
+        <p>Placeholder for the contact form.</p>
+    </x-card>
+</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello. World!</h1>
+<x-layout >
+    <h1>Hello, World!</h1>
 
-    <div>
-        <a href="/about">About Us</a>
-        <br>
-        <a href="/contact">Contact</a>
-    </div>
-</body>
-</html>
+</x-layout>
