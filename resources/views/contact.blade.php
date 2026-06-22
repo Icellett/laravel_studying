3daf18b7d96d883fa343be5f511de1ecd8dd7ea1
Anna Sankova <anya.sankova.05@gmail.com>
2026-06-22 14:37:39 +0600

    Routing 101

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contact</h1>
+    <p> <b>Author:</b> Icellet</p>
+    <p> <b>Email:</b> test.email@gmail.com</p>
+
+    <div>
+        <a href="/">Return Home</a>
+    </div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello. World!</h1>
+
+    <div>
+        <a href="/about">About Us</a>
+        <br>
+        <a href="/contact">Contact</a>
+    </div>
+</body>
+</html>
